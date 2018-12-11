ab3af9dcdf04d3372dbb82078e872adac7f40dca
Herman Schaaf <herman.schaaf@skyscanner.net>
2018-12-11 08:41:37 +0000

    Small improvement

--- day11/part2.py
+++ day11/part2.py
@@ -13,15 +13,14 @@ for y in range(1, n+1):
         grid[y-1][x-1] = power
 
 mx, mx_score = None, None
-sizes = [[0 for c in range(n)] for r in range(n)]
-
 for y in range(1, n+1):
     for x in range(1, n+1):
+        size = 0
         for s in range(1, 300-max(x, y)+1):
             g = sum(grid[y+s-1][x+c] for c in range(s))
             g += sum(grid[y+r][x+s-1] for r in range(s))
-            score = sizes[y-1][x-1] + g
-            sizes[y-1][x-1] = score
+            score = size + g
+            size = score
             if mx_score is None or score > mx_score:
                 mx_score = score
                 mx = (x+1, y+1, s)
