4cd71709bb4ce7534e342ebf5685160e9c7db775
Herman Schaaf <herman.schaaf@skyscanner.net>
2018-12-18 08:31:40 +0000

    Day 18

--- day18/day18.py
+++ day18/day18.py
@@ -26,10 +26,16 @@ def adj(grid, r, c):
     return cnt
 
 
-for step in range(0, 10):
+def print_grid(grid):
+    for row in grid:
+        print("".join(row))
+    print("")
+
+
+def run_step(grid):
     ngrid = [[grid[row][col] for col in range(cols)] for row in range(rows)]
     for r in range(rows):
-        for c in range(rows):
+        for c in range(cols):
             val = grid[r][c]
             counts = adj(grid, r, c)
             if val == '.' and counts["|"] >= 3:
@@ -38,7 +44,42 @@ for step in range(0, 10):
                 ngrid[r][c] = '#'
             elif val == '#' and counts['#'] >= 1 and counts['|'] >= 1:
                 ngrid[r][c] = '#'
-    grid = ngrid
-    for row in grid:
-        print("".join(row))
-    print("")
+            elif val == '#':
+                ngrid[r][c] = '.'
+    return ngrid
+
+
+def score(grid):
+    wood = sum(1 for r in range(rows) for c in range(cols) if grid[r][c] == '|')
+    lumber = sum(1 for r in range(rows) for c in range(cols) if grid[r][c] == '#')
+    return wood * lumber
+
+
+def hash(grid):
+    return "".join("".join(row) for row in grid)
+
+
+# Part 1
+target = 1000000000
+lastseen = dict()
+cycle = None
+step = 1
+while True:
+    grid = run_step(grid)
+
+    h = hash(grid)
+    if h not in lastseen:
+        lastseen[h] = step
+    else:
+        cycle = step - lastseen[h]
+        steps = (target - step) // cycle
+        step += steps * cycle
+        lastseen[h] = step
+
+    if step == 10:
+        print("Part 1:", score(grid))
+    if step == target:
+        print("Part 2:", score(grid))
+        break
+
+    step += 1
